abcf5ef2ba3c44524a2277d7eeeda2428d4af32f
giorgioventurin <giorgioventurin5@gmail.com>
2022-06-07 15:30:06 +0200

    Morphological operators - added comparisons with canny edge detector.

--- Morphological/src/morphological.cpp
+++ Morphological/src/morphological.cpp
@@ -7,20 +7,33 @@
 
 struct user_data{
     cv::Mat img;
+    cv::Mat mask;
     int* iters_dil = nullptr;
     int* iters_ero = nullptr;
+    int* t1 = nullptr;
+    int* t2 = nullptr;
 };
 
 static void on_trackbar(int event, void* userdata) {
 
     user_data data = *(struct user_data*) userdata;
+
     int i_dil = *data.iters_dil;
     int i_ero = *data.iters_ero;
     cv::Mat img = data.img;
+    cv::Mat mask = data.mask;
+    int t1 = *data.t1;
+    int t2 = *data.t2;
+
+    cv::Mat canny, img_gray;
+    cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
+
+    cv::Canny(img_gray, canny, t1, t2);
 
     cv::Mat eroded;
     cv::Mat dilated;
-    //standard erosion kernel
+
+    //standard kernel
     cv::Mat kernel = cv::Mat::zeros(3,3,CV_8UC1);
     kernel.at<unsigned char>(0,1) = 1;
     kernel.at<unsigned char>(1,0) = 1;
@@ -28,16 +41,25 @@ static void on_trackbar(int event, void* userdata) {
     kernel.at<unsigned char>(1,2) = 1;
     kernel.at<unsigned char>(2,1) = 1;
 
-    cv::erode(img, eroded, kernel, cv::Point(-1,1), i_ero);
-    cv::dilate(img, dilated, kernel, cv::Point(-1,1), i_dil);
+    cv::erode(canny, eroded, kernel, cv::Point(-1,1), i_ero);
+    cv::dilate(canny, dilated, kernel, cv::Point(-1,1), i_dil);
     cv::imshow("Erosion", eroded);
     cv::imshow("Dilation", dilated);
 
-    cv::Mat er_dil, dil_er;
-    cv::dilate(eroded, er_dil, kernel, cv::Point(-1,1), i_dil);
-    cv::erode(dilated, dil_er, kernel, cv::Point(-1,1), i_ero);
-    cv::imshow("Opening", er_dil);
-    cv::imshow("Closing", dil_er);
+    cv::Mat open, close;
+    cv::dilate(eroded, open, kernel, cv::Point(-1,1), i_dil);
+    cv::erode(dilated, close, kernel, cv::Point(-1,1), i_ero);
+    cv::imshow("Opening", open);
+    cv::imshow("Closing", close);
+
+    cv::Mat canny_mask, canny_close;
+
+    cv::Canny(mask, canny_mask, t1, t2);
+    cv::Canny(close, canny_close, t1, t2);
+
+    cv::imshow("Canny on image", canny);
+    cv::imshow("Canny on mask", canny_mask);
+    cv::imshow("Canny on closing", canny_close);
 
 }
 int main() {
@@ -88,23 +110,32 @@ int main() {
 
     cv::imshow("Mask", mask);
 
-    cv::Mat eroded;
-
-
     int dil_iterations = 1;
     int ero_iterations = 1;
+    int th1 = 1;
+    int th2 = 1;
+
     std::string trackbar_ero = "Erosion iterations";
     std::string trackbar_dil = "Dilation iterations";
+    std::string trackbar_t1 = "Threshold 1";
+    std::string trackbar_t2 = "Threshold 2";
+
     cv::namedWindow("Erosion");
     cv::namedWindow("Dilation");
     cv::namedWindow("Opening");
     cv::namedWindow("Closing");
+    cv::namedWindow("Canny on image");
+    cv::namedWindow("Canny on mask");
+    cv::namedWindow("Canny on closing");
 
 
     user_data data;
-    data.img = mask;
+    data.img = img;
+    data.mask = mask;
     data.iters_ero = &ero_iterations;
     data.iters_dil = &dil_iterations;
+    data.t1 = &th1;
+    data.t2 = &th2;
 
     cv::createTrackbar(trackbar_ero, "Erosion", &ero_iterations, 100, on_trackbar, (void*)&data);
 
@@ -115,6 +146,15 @@ int main() {
 
     cv::createTrackbar(trackbar_ero, "Closing", &ero_iterations, 100, on_trackbar, (void*)&data);
     cv::createTrackbar(trackbar_dil, "Closing", &dil_iterations, 100, on_trackbar, (void*)&data);
+
+    cv::createTrackbar(trackbar_t1, "Canny on image", &th1, 300, on_trackbar, (void*)&data);
+    cv::createTrackbar(trackbar_t2, "Canny on image", &th2, 300, on_trackbar, (void*)&data);
+
+    cv::createTrackbar(trackbar_t1, "Canny on mask", &th1, 300, on_trackbar, (void*)&data);
+    cv::createTrackbar(trackbar_t2, "Canny on mask", &th2, 300, on_trackbar, (void*)&data);
+
+    cv::createTrackbar(trackbar_t1, "Canny on closing", &th1, 300, on_trackbar, (void*)&data);
+    cv::createTrackbar(trackbar_t2, "Canny on closing", &th2, 300, on_trackbar, (void*)&data);
 
     cv::waitKey(0);
     return 0;
