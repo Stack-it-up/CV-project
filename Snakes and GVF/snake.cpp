57181d49f706a1ec9cb83bb7f43ef294eeed0d10
Filippo Boldrin <filippo.boldrin@studenti.unipd.it>
2022-07-22 19:02:48 +0200

    snakes completed (not working, needs more testing)

--- Snakes and GVF/snake.cpp
+++ Snakes and GVF/snake.cpp
@@ -14,21 +14,52 @@
 using namespace cv;
 using namespace std;
 
-Mat create_A(int K, double alpha, double beta); //forward declaration
+void create_A(cv::Mat& A, int K, double alpha, double beta); //forward declaration
 
-void compute_snake(vector<Point> const& initial_contour, Mat const& Fx, Mat const& Fy, double alpha, double beta, double gamma, int iters=500) {
-    int K = initial_contour.size();
-    Mat A = create_A(K, alpha, beta);
+void compute_snake(vector<Point> const& initial_contour, Mat const& ext_force_x, Mat const& ext_force_y, double alpha, double beta, double gamma, int iters=500) {
+    CV_Assert(initial_contour.size() >= 6); //(otherwise, it won't work for the matrix D4)
+    CV_Assert(ext_force_y.type()==CV_64FC1 && ext_force_y.type()==CV_64FC1);
+    CV_Assert(alpha>0 && beta>0 && gamma>0);
+
+    int K = initial_contour.size() - 1;
+    Mat A{};
+    create_A(A, K, alpha, beta);
+
+    //create the vectors x and y to be premultiplied later
+    //x_t contains all the x's of the points in the contour
+    Mat x_t{K, 1, CV_16UC1};
+    Mat y_t{K, 1, CV_16UC1};
+    for(int index=0; index<K; index++) {
+        Point p = initial_contour[index];
+        x_t.at<ushort>(index) = p.x;
+        y_t.at<ushort>(index) = p.y;
+    }
+
+    for(int t=1; t<iters; t++) {
+        //create and initialize Fx and Fy
+        Mat Fx{K, 1, CV_64FC1};
+        Mat Fy{K, 1, CV_64FC1};
+        for(int i=0; i<K; i++) {
+            int x_coord = x_t.at<ushort>(i);
+            int y_coord = y_t.at<ushort>(i);
+            Fx.at<double>(i) = ext_force_x.at<double>(x_coord, y_coord);
+            Fy.at<double>(i) = ext_force_y.at<double>(x_coord, y_coord);
+        }
+        //compute the new vector
+        x_t = A * (x_t + gamma*Fx);
+        y_t = A * (y_t + gamma*Fy);
+        //maybe TODO implement early stopping based on convergence rate?
+    }
 }
 
 //create the matrix A = [I-D]^-1
-Mat create_A(int K, double alpha, double beta) {
-    Mat D2 = Mat::zeros(Size{K,K}, CV_64FC1);
-    Mat D4 = D2.clone();
+void create_A(cv::Mat& A, int K, double alpha, double beta) {
+    Mat D2{Size{K,K}, CV_64FC1, 0.0};
+    Mat D4{Size{K,K}, CV_64FC1, 0.0};
 
     //initialize D2
     for(int row=0; row<K; row++) { //row index and anchor index (point whose value is -2 in the row)
-        int prev = (row-1) % K;
+        int prev = (row-1+K) % K;
         int succ = (row+1) % K;
 
         D2.at<double>(row, prev) = 1;
@@ -37,9 +68,9 @@ Mat create_A(int K, double alpha, double beta) {
     }
 
     //similarly, inizialize D4
-    for(int row=0; row<K; row++) { //row index and anchor index (point whose value is -2 in the row)
-        int prev2 = (row-2) % K;
-        int prev = (row-1) % K;
+    for(int row=0; row<K; row++) { //row index and anchor index (point whose value is 6 in the row)
+        int prev2 = (row-2+K) % K;
+        int prev = (row-1+K) % K;
         int succ = (row+1) % K;
         int succ2 = (row+2) % K;
 
@@ -50,26 +81,27 @@ Mat create_A(int K, double alpha, double beta) {
         D4.at<double>(row, succ2) = 1;
     }
 
+
     //weighted sum of the two matrices
-    Mat D{Size{K,K}, CV_64FC1};
-    addWeighted(D2, alpha, D4, -beta, 0, D);
+    Mat D = alpha*D2 - beta*D4;
 
     //create A as I-D and invert it.
     // I is the identity matrix
-    Mat A = Mat::eye(Size{K,K}, CV_64FC1) - D;
-    A = A.inv(DECOMP_LU);    //using LU decomposition is highly efficient in this case because D is a 5-banded matrix
+    Mat tmp = Mat::eye(Size{K,K}, CV_64FC1) - D;
+    //cout << A <<'\n';
+    tmp = tmp.inv(DECOMP_LU);    //using LU decomposition is highly efficient in this case because D is a 5-banded matrix
 
-    return A;
+    tmp.copyTo(A);
 }
 
 int main() {
-    /*
+
     string DIR = "/home/filippo/Desktop/test/*.jpeg";
     vector<string> imgdirs;
     glob(DIR, imgdirs);
 
     vector<Mat> imgs(imgdirs.size());
-    for(int i=0; i<imgdirs.size(); i++) {
+    for (int i = 1; i < imgdirs.size(); i++) {
         imgs[i] = imread(imgdirs[i]);
         cvtColor(imgs[i], imgs[i], COLOR_BGR2GRAY);
         //-------------------------------------------------------
@@ -78,11 +110,12 @@ int main() {
         //-------------------------------------------------------
         vector<vector<Point>> contours;
         vector<Vec4i> hierarchy;
-        findContours( roi, contours, hierarchy,
-                      RETR_LIST, CHAIN_APPROX_SIMPLE );
+        findContours(roi, contours, hierarchy,
+                     RETR_LIST, CHAIN_APPROX_SIMPLE);
         cvtColor(roi, roi, COLOR_GRAY2BGR);
         int idx = hierarchy[0][0];
         RNG rand{getTickCount()};
+        /*
         while(idx >= 0) {
             Scalar color{rand(256), rand(256), rand(256)};
             drawContours(roi, contours, idx, color, 5, 8, hierarchy);
@@ -90,23 +123,26 @@ int main() {
         }
         imshow("", roi);
         waitKey();
+         */
 
-        */
 
-        /*
         Mat vfc_x;
         Mat vfc_y;
-
         int ker_size = min(imgs[i].rows, imgs[i].cols);
         ker_size /= 2;
-        if(ker_size%2 == 0)
+        if (ker_size % 2 == 0)
             ker_size += 1;
         compute_vfc(imgs[i], vfc_x, vfc_y, 101, 2.4);
-         */
+
+        /*
         vector<Point> fake_snake{};
-        for(int i = 0; i<5; i++)
+        for(int i = 0; i<8; i++)
             fake_snake.emplace_back(i,i); //Point() is called by emplace_back!
         fake_snake.emplace_back(0,0); //to make it closed!
+        */
 
-        compute_snake(fake_snake, Mat{}, Mat{}, 0.5, 0.5, 0);
+        compute_snake(contours[2], vfc_x, vfc_y, 0.5, 0.5, 0.5);
+        Scalar color{rand(256), rand(256), rand(256)};
+        drawContours(roi, contours[2], -1, color);
+    }
 }
