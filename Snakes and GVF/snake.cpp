c93bc182e8d3d403212e520ef9a5293bc7dab667
Filippo Boldrin <filippo.boldrin@studenti.unipd.it>
2022-07-24 17:29:39 +0200

    refactoring to make the code almost decent

--- Snakes and GVF/snake.cpp
+++ Snakes and GVF/snake.cpp
@@ -9,126 +9,15 @@
 #include <vector>
 #include <iostream>
 
-#include "gvf.h"
+#include "snakes.h"
 
 using namespace cv;
 using namespace std;
 
-void create_A(cv::Mat& A, int K, double alpha, double beta); //forward declaration
-
-void compute_snake(vector<Point> & initial_contour, Mat const& ext_force_x, Mat const& ext_force_y, double alpha, double beta, double gamma, int iters=500) {
-    CV_Assert(initial_contour.size() >= 6); //(otherwise, it won't work for the matrix D4)
-    CV_Assert(ext_force_y.type()==CV_64FC1 && ext_force_y.type()==CV_64FC1);
-    CV_Assert(alpha>0 && beta>0 && gamma>0);
-
-    int K = initial_contour.size();
-    Mat A{};
-    create_A(A, K, alpha, beta);
-
-    //create the vectors x and y to be premultiplied later
-    //x_t contains all the x's of the points in the contour
-    Mat x_t(K, 1, CV_64FC1); //even though only integers should be inside here
-    Mat y_t(K, 1, CV_64FC1); //idem
-    for(int index=0; index<K; index++) {
-        Point p = initial_contour.at(index);
-        x_t.at<double>(index) = static_cast<double>(p.x);
-        y_t.at<double>(index) = static_cast<double>(p.y);
-    }
-
-    //cerr << "K: " << K << "\n";
-    Mat Fx = Mat::zeros(K, 1, CV_64FC1);
-    Mat Fy = Mat::zeros(K, 1, CV_64FC1);
-    for(int t=1; t<iters; t++) {
-        //cerr << "t: " << t << "\n";
-        //cerr << "x_t size: " << x_t.size() << "\n";
-        //cerr << "y_t size: " << y_t.size() << "\n";
-        //initialize Fx and Fy
-        for(int i=0; i<K; i++) {
-            int x_coord = cvRound(x_t.at<double>(i));
-            if(x_coord < 0)
-                x_coord = 0;
-            else if(x_coord > ext_force_x.cols)
-                x_coord = ext_force_x.cols;
-            //cerr << "i: " << i << "\n";
-            //cerr << "x_coord: " << x_coord << "\n";
-            int y_coord = cvRound(y_t.at<double>(i));
-            if(y_coord < 0)
-                y_coord = 0;
-            else if(y_coord > ext_force_x.rows)
-                y_coord = ext_force_x.rows;
-            //cerr << "y_coord: " << y_coord << "\n";
-            Fx.at<double>(i) = ext_force_x.at<double>(y_coord, x_coord);
-            Fy.at<double>(i) = ext_force_y.at<double>(y_coord, x_coord);
-        }
-        //compute the new vector
-        x_t = A * (x_t + gamma*Fx);
-        //cerr << A;
-        //cerr << x_t << "\n";
-        y_t = A * (y_t + gamma*Fy);
-        //maybe TODO implement early stopping based on convergence rate?
-    }
-    //write out the definitive contours
-    for(int index=0; index<K; index++) {
-        int px = cvRound(x_t.at<double>(index));
-        int py = cvRound(y_t.at<double>(index));
-        initial_contour[index] = Point{px, py};
-    }
-}
-
-//create the matrix A = [I-D]^-1
-void create_A(cv::Mat& A, int K, double alpha, double beta) {
-    Mat D2{Size{K,K}, CV_64FC1, 0.0};
-    Mat D4{Size{K,K}, CV_64FC1, 0.0};
-
-    //initialize D2
-    for(int row=0; row<K; row++) { //row index and anchor index (point whose value is -2 in the row)
-        int prev = (row-1+K) % K;
-        int succ = (row+1) % K;
-
-        D2.at<double>(row, prev) = 1;
-        D2.at<double>(row,row) = -2;
-        D2.at<double>(row, succ) = 1;
-    }
-
-    //similarly, inizialize D4
-    for(int row=0; row<K; row++) { //row index and anchor index (point whose value is 6 in the row)
-        int prev2 = (row-2+K) % K;
-        int prev = (row-1+K) % K;
-        int succ = (row+1) % K;
-        int succ2 = (row+2) % K;
-
-        D4.at<double>(row, prev2) = 1;
-        D4.at<double>(row, prev) = -4;
-        D4.at<double>(row,row) = 6;
-        D4.at<double>(row, succ) = -4;
-        D4.at<double>(row, succ2) = 1;
-    }
-
-    //weighted sum of the two matrices
-    Mat D = alpha*D2 - beta*D4;
-
-    //create A as I-D and invert it.
-    // I is the identity matrix
-    Mat tmp = Mat::eye(Size{K,K}, CV_64FC1) - D;
-    //cout << A <<'\n';
-    tmp = tmp.inv(DECOMP_LU);    //using LU decomposition is highly efficient in this case because D is a 5-banded matrix
-
-    tmp.copyTo(A);
-}
-
-void greedy_snake(vector<Point> & initial_contour,
-                  Mat const& ext_force_x,
-                  Mat const& ext_force_y,
-                  double alpha, double beta, double gamma,
-                  int iters=500)
-                  {
-    Mat A;
-}
-
 int main() {
     namedWindow("win", WINDOW_NORMAL);
 
-    Mat beer = imread("/home/filippo/Desktop/test/hand.png", IMREAD_COLOR);
+    Mat beer = imread("/home/filippo/Desktop/test/hand.jpg", IMREAD_COLOR);
     Mat beer_gray;
     cvtColor(beer, beer_gray, COLOR_BGR2GRAY);
     Mat beer2 = imread("/home/filippo/Desktop/test/hand_cont.png", IMREAD_GRAYSCALE);
@@ -141,23 +30,20 @@ int main() {
     Mat circle_gray;
     cvtColor(circle, circle_gray, COLOR_BGR2GRAY);
     */
-    Mat vfc_x;
-    Mat vfc_y;
-    int ker_size = min(beer.rows, beer.cols);
+    Mat vfc_x, vfc_y;
+    int ker_size = max(beer.rows, beer.cols);
     //ker_size /= 2;
     if (ker_size % 2 == 0)
         ker_size -= 1;
-    compute_vfc(beer_gray, vfc_x, vfc_y, 501, 2.4);
+    VFC(beer_gray, vfc_x, vfc_y, 101, 2.4);
 
-    imshow("vfc_x", vfc_x);
-    imshow("vfc_y", vfc_y);
+    Mat mog_x, mog_y;
+    MOG(beer_gray, mog_x, mog_y);
+
+    imshow("mog_x", mog_x);
+    imshow("mog_y", mog_y);
     waitKey();
-    /*
-    vector<Point> fake_snake{};
-    for(int i = 0; i<8; i++)
-        fake_snake.emplace_back(i,i); //Point() is called by emplace_back!
-    fake_snake.emplace_back(0,0); //to make it closed!
-    */
+
     Scalar RED{0,0,255};
     Scalar GREEN{0, 255, 0};
 
@@ -167,11 +53,11 @@ int main() {
     imshow("win", beer);
     waitKey();
 
-    compute_snake(contours[0], vfc_x, vfc_y, 0.7, 2, 4, 1000);
+    compute_snake(contours[0], mog_x, mog_y, 2, 3, 5, 1000);
     //subsample the contour
     vector<Point> subsampled;
     for(int j=0; j<contours[0].size(); j++) {
-        if(j % 5 == 0)
+        if(j % 2 == 0)
             subsampled.push_back(contours[0][j]);
     }
     contours[0] = subsampled;
---
+++ Snakes and GVF/snakes.cpp
@@ -0,0 +1,226 @@
+//
+// Created by filippo on 24/07/22.
+//
+
+#include "snakes.h"
+
+using namespace cv;
+using namespace std;
+
+void create_A(cv::Mat& A, int K, double alpha, double beta) {
+    //create the matrix A = [I-D]^-1 to be used in compute_snake()
+
+    //D2 and D4 are matrices of zeros
+    Mat D2{Size{K,K}, CV_64FC1, 0.0};
+    Mat D4{Size{K,K}, CV_64FC1, 0.0};
+
+    //initialize D2
+    for(int row=0; row<K; row++) { //row index and anchor index (point whose value is -2 in the row)
+        int prev = (row-1+K) % K;
+        int succ = (row+1) % K;
+
+        D2.at<double>(row, prev) = 1;
+        D2.at<double>(row,row) = -2;
+        D2.at<double>(row, succ) = 1;
+    }
+
+    //similarly, inizialize D4
+    for(int row=0; row<K; row++) { //row index and anchor index (point whose value is 6 in the row)
+        int prev2 = (row-2+K) % K;
+        int prev = (row-1+K) % K;
+        int succ = (row+1) % K;
+        int succ2 = (row+2) % K;
+
+        D4.at<double>(row, prev2) = 1;
+        D4.at<double>(row, prev) = -4;
+        D4.at<double>(row,row) = 6;
+        D4.at<double>(row, succ) = -4;
+        D4.at<double>(row, succ2) = 1;
+    }
+
+    //weighted sum of the two matrices
+    Mat D = alpha*D2 - beta*D4;
+
+    //create A as I-D and invert it.
+    // I is the identity matrix
+    Mat tmp = Mat::eye(Size{K,K}, CV_64FC1) - D;
+    //cout << A <<'\n';
+    tmp = tmp.inv(DECOMP_LU);    //using LU decomposition is highly efficient in this case because D is a 5-banded matrix
+
+    tmp.copyTo(A);
+}
+
+void compute_snake(vector<Point> & contour, Mat const& ext_force_x, Mat const& ext_force_y, double alpha, double beta, double gamma, int iters=500) {
+    CV_Assert(contour.size() >= 6); //(otherwise, it won't work for the matrix D4)
+    CV_Assert(ext_force_y.type()==CV_64FC1 && ext_force_y.type()==CV_64FC1);
+    CV_Assert(alpha>0 && beta>0 && gamma>0);
+
+    int K = contour.size(); //K is the number of points in the contour
+    Mat A{};
+    create_A(A, K, alpha, beta);
+
+    //create the vectors x and y to be premultiplied later
+    //x_t contains all the x's of the points in the contour
+    Mat x_t(K, 1, CV_64FC1); //needed for matrix multiplication, even though only integers should be inside here
+    Mat y_t(K, 1, CV_64FC1);
+    for(int index=0; index<K; index++) {
+        Point p = contour.at(index);
+        x_t.at<double>(index) = static_cast<double>(p.x);
+        y_t.at<double>(index) = static_cast<double>(p.y);
+    }
+
+    Mat Fx = Mat::zeros(K, 1, CV_64FC1);
+    Mat Fy = Mat::zeros(K, 1, CV_64FC1);
+    for(int t=1; t<iters; t++) {
+        //initialize Fx and Fy
+        for(int i=0; i<K; i++) {
+            int x_coord = cvRound(x_t.at<double>(i));
+            if(x_coord < 0)
+                x_coord = 0;
+            else if(x_coord > ext_force_x.cols)
+                x_coord = ext_force_x.cols;
+            int y_coord = cvRound(y_t.at<double>(i));
+            if(y_coord < 0)
+                y_coord = 0;
+            else if(y_coord > ext_force_x.rows)
+                y_coord = ext_force_x.rows;
+
+
+            Fx.at<double>(i) = ext_force_x.at<double>(y_coord, x_coord);
+            Fy.at<double>(i) = ext_force_y.at<double>(y_coord, x_coord);
+        }
+        //compute the new vector
+        x_t = A * (x_t + gamma*Fx);
+        y_t = A * (y_t + gamma*Fy);
+    }
+    //write out the final contour
+    for(int index=0; index<K; index++) {
+        int px = cvRound(x_t.at<double>(index));
+        int py = cvRound(y_t.at<double>(index));
+        contour[index] = Point{px, py};
+    }
+}
+
+void MOG(Mat& input, Mat& output_x, Mat& output_y) {
+    Mat dx = Mat{input.size(), CV_64F};
+    Mat dy = dx.clone();
+    Mat abs_dx, abs_dy;
+
+    Sobel(input, dx, CV_64F, 1, 0, 5);
+    Sobel(input, dy, CV_64F, 0, 1, 5);
+    Mat mag{dx.size(), CV_64F};
+
+    //use the squared norm of the gradient vector
+    mag = dx.mul(dx) + dy.mul(dy);
+
+    Sobel(mag, dx, CV_64F, 1, 0);
+    Sobel(mag, dy, CV_64F, 0, 1);
+
+    //normalize
+    Mat F_mag;
+    F_mag = abs(dx) + abs(dy); //L1 norm
+    F_mag += 1e-05;
+    dx /= F_mag;
+    dy /= F_mag;
+
+    //return
+    dx.copyTo(output_x);
+    dy.copyTo(output_y);
+}
+
+void VFC(Mat const& input, Mat& output_x, Mat& output_y, int k, double gamma) {
+    CV_Assert(k%2 == 1); //we nee an odd-sized kernel!
+    CV_Assert(gamma>0);
+    constexpr double EPSILON = 1e-05;
+
+    Mat magnitude{};
+    CV_Assert(input.type() == CV_8UC1);
+
+    Mat dx = Mat{input.size(), CV_64F};
+    Mat dy = dx.clone();
+    Mat abs_dx, abs_dy;
+
+    spatialGradient(input, dx, dy);
+    Mat mag{dx.size(), CV_64F};
+
+    //use L1 approximation of gradient magnitude
+    addWeighted(abs(dx), 0.5, abs(dy), 0.5, 0, magnitude);
+    //-----------------------------------------------
+
+    //now that we have the gradient magnitude:
+    Mat nx{Size(k,k), CV_64FC1};
+    Mat ny = nx.clone();
+
+    Mat m{Size(k,k), CV_64FC1}; //m1(x,y) = (r+EPSILON)^-gamma
+
+    //fill the nx, ny, m matrices
+    for(int x=-k/2; x<=k/2; x++) { //col = x + k/2
+        for(int y=-k/2; y<=k/2; y++) {//row = y + k/2
+            if(x==0 && y==0) {
+                nx.at<double>(k/2,k/2) = 0;
+                ny.at<double>(k/2,k/2) = 0;
+                continue;
+            }
+            int col = x + k/2;
+            int row = y + k/2;
+            double r = sqrt(x*x + y*y);
+
+            nx.at<double>(row, col) = -x/r;
+            ny.at<double>(row, col) = -y/r;
+
+            m.at<double>(row, col) = pow((r+EPSILON), -gamma);
+        }
+    }
+
+    Mat kx{Size(k,k), CV_64FC1};
+    Mat ky = kx.clone();
+
+    //elementwise multiplication
+    kx = m.mul(nx);
+    ky = m.mul(ny);
+
+    //now apply the filtering!! (internally uses the DFT)
+    filter2D(magnitude, output_x, CV_64F, kx);
+    filter2D(magnitude, output_y, CV_64F, ky);
+
+    //normalize the force using L1 norm (cfr. Gonzalez 11-49)
+    output_x = -output_x / (abs(output_x)+abs(output_y)+1e-05);
+    output_y = -output_y / (abs(output_x)+abs(output_y)+1e-05);
+}
+
+vector<Point> contour_from_rect(Rect bbox, int step) {
+    //drawing the contour clockwise (NW -> NE -> SE -> SW)
+    vector<Point> contour;
+
+    int x,y;
+    contour.emplace_back(bbox.x, bbox.y); //add the first point
+
+    x = bbox.x + step;
+    while(x <= bbox.x + bbox.width) {
+        contour.emplace_back(x, bbox.y);
+        x += step;
+    }
+
+    x = bbox.x + bbox.width;
+    y = bbox.y + step;
+    while(y <= bbox.y + bbox.height) {
+        contour.emplace_back(x, y);
+        y += step;
+    }
+
+    x = bbox.x + bbox.width - step;
+    y = bbox.y + bbox.height;
+    while(x >= bbox.x) {
+        contour.emplace_back(x, y);
+        x -= step;
+    }
+
+    x = bbox.x;
+    y = bbox.y + bbox.height - step;
+    while(y >= bbox.y) {
+        contour.emplace_back(x, y);
+        y -= step;
+    }
+}
+
+
